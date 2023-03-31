a5a6cb73c1abcc1081c4bae65db46937667836d0
19350308 <isc19350308@gmail.com>
2023-03-31 09:09:26 -0600

    Estructura de Lista de compras

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+function App() {
+
+  return (
+    <div className="container text-center">
+      <div className="row">
+        <div className="col">
+        <h1>Shopping List</h1>
+        <hr />
+        </div>
+      </div>
+      <div className="row">
+      <div className="col">C</div>
+      <div className="col-8"> Nombres Item</div>
+      <div className="col-3"> Botones </div>
+      </div>
+      <div className="row">
+      <div className="col">C</div>
+      <div className="col-8"> Nombres Item</div>
+      <div className="col-3"> Botones </div>
+      </div>
+      <div className="row">
+      <div className="col">C</div>
+      <div className="col-8"> Nombres Item</div>
+      <div className="col-3"> Botones </div>
+      </div>
+  </div>
+  )
+
+}
+
+export default App
